14971c8bd4c7b7d2cd172762baaf53d4f0457c93
kaveh <lcaveh@gmail.com>
2025-12-16 10:59:02 -0800

    Update Autocomplete id

--- src/components/Solutions/AutoSuggest/meta.ts
+++ src/components/Solutions/AutoSuggest/meta.ts
@@ -1,7 +1,7 @@
 import { SolutionMeta } from '@configs/solutions';
 
 const meta: SolutionMeta = {
-    id: 2,
+    id: 3,
     title: 'Autocomplete Input',
     description: 'Build An autocomplete input that filters and suggests matching options as the user types.',
     image: '/assets/auto-complete-input.png',
